61136568d28261459ee58a2b25be38a3bdec0d03
Matheus Lopes <msldasilva@gmail.com>
2021-08-17 18:28:46 -0300

    Adding log to all database operations

--- loaders/database.js
+++ loaders/database.js
@@ -1,7 +1,12 @@
 import mongoose from 'mongoose';
+import { logger } from '../config/logger.js';
 
 export default class Database {
     async load() {
+        mongoose.set('debug', function(collection, method, ...args) {           
+            logger.database(`collection: ${collection} | method: ${method} | args: ${JSON.stringify(args)}`);
+        });
         await mongoose.connect(process.env.DATABASE_URL);
+        
     }
 }
